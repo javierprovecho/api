0472ddbcb0a556f5cddf17dfbb17509cc4d1161e
Javier Provecho <j.provecho@dartekstudios.com>
2014-04-12 18:33:41 +0200

    Update bbdd.php
    Ahora se usa conector mysqli.

--- lib/bbdd.php
+++ lib/bbdd.php
@@ -1,14 +1,14 @@
 <?php
 // Conexion a MYSQL
-$db = @mysql_pconnect("localhost", "xxxxxxxx", "xxxxxxxx");
-if(!$db) {
+$db = new mysqli("localhost", "xxxxxxxx", "xxxxxxxx");
+if(mysqli_connect_errno()) {
    $json = array('status' => 11);
    echo json_encode($json,JSON_UNESCAPED_UNICODE);
    exit;
 }
 
 // Seleccion de la tabla
-if(!@mysql_select_db("alergant")) {
+if(!$db->select_db("alergant")) {
    $json = array('status' => 12);
    echo json_encode($json,JSON_UNESCAPED_UNICODE);
    exit;
